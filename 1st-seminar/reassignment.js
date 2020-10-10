d6f041d12ffb160eb58aaee2da6b19177703a2c1
Woo-yeong Kim <kimwy1997@gmail.com>
2020-10-10 17:15:29 +0900

    first commit

---
+++ 1st-seminar/reassignment.js
@@ -0,0 +1,9 @@
+let variableLet = "123";
+variableLet = "321";
+
+console.log(`variableLet: ${variableLet}`);
+
+const variableConst = "123";
+variableConst = "321";
+
+console.log(`variableConst: ${variableConst}`);
---
+++ 1st-seminar/scope.js
@@ -0,0 +1,19 @@
+if (true) {
+  var x = "var";
+}
+console.log(`var: ${x}`);
+
+if (true) {
+  let y = "let";
+}
+// console.log(`let: ${y}`);
+
+function colorFunction() {
+  if (true) {
+    var color = "blue";
+    console.log(color);
+  }
+  console.log(color);
+}
+colorFunction();
+console.log(color);
