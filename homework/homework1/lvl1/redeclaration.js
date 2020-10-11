48edbb74b49de322de24dc35691c29fab9e3b56e
Woo-yeong Kim <kimwy1997@gmail.com>
2020-10-12 00:47:13 +0900

    Fix directories

---
+++ homework/homework1/lvl1/redeclaration.js
@@ -0,0 +1,13 @@
+var vv = "123";
+console.log(vv);
+
+var vv = "321";
+console.log(vv);
+
+let variableLet = "123";
+let variableLet = "321";
+console.log(`variableLet : ${variableLet}`);
+
+const variableConst = "123";
+const variableConst = "321";
+console.log(variableConst);
