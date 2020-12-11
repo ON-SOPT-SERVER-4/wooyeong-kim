fda0b3fe49d6e707111f1d89fb59f67510fb00d4
Woo-yeong Kim <kimwy1997@gmail.com>
2020-12-11 23:51:37 +0900

    Upload data for 6th-seminar

---
+++ 6th-seminar/routes/users/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../../controller/userController");
+
+const authUtils = require("../../middlewares/authUtils");
+
+router.post("/signup", userController.signup);
+router.post("/signin", userController.signin);
+router.get("/", authUtils.checkToken, userController.readAll);
+router.get("/profile", authUtils.checkToken, userController.getProfile);
+router.get("/:id", userController.readOne);
+
+module.exports = router;
